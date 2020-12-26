9d6c242880f9d0020c6ea7843c66784152907dd6
LucianoNeimark <38950566+LucianoNeimark@users.noreply.github.com>
2020-12-26 19:16:19 -0300

    Check Admin
    Middleware que revisa que guido sea el que esta haciendo los cambios importantes funciona.

--- backend/api/routes/monedas.js
+++ backend/api/routes/monedas.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Moneda = require("../models/moneda");
 const mongoose = require("mongoose");
+const checkAdmin = require("../../middleware/checkAdm");
 
 const monedaControllers = require("../controllers/monedas");
 
-router.post("/", monedaControllers.moneda_crear);
+router.post("/", checkAdmin, monedaControllers.moneda_crear);
 router.get("/", monedaControllers.moneda_get_all);
 router.get("/:nombre", monedaControllers.moneda_get_one);
-router.delete("/:nombre", monedaControllers.moneda_delete_nombre);
-router.patch("/:nombre", monedaControllers.moneda_modificar_valor);
+router.delete("/:nombre", checkAdmin, monedaControllers.moneda_delete_nombre);
+router.patch("/:nombre", checkAdmin, monedaControllers.moneda_modificar_valor);
 
 module.exports = router;
