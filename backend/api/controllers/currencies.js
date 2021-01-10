9c38b808c392a9d7a469aef57b4bb428b0262674
LucianoNeimark <38950566+LucianoNeimark@users.noreply.github.com>
2021-01-10 11:00:48 -0300

    Get OrderNumber fixed

--- backend/api/controllers/currencies.js
+++ backend/api/controllers/currencies.js
@@ -8,7 +8,7 @@ exports.currency_get_all = (req, res, next) => {
     .exec()
     .then((docs) => {
       const response = {
-         docs.map((doc) => {
+        currencies: docs.map((doc) => {
           return {
             doc,
           };
--- backend/api/routes/orders.js
+++ backend/api/routes/orders.js
@@ -6,7 +6,7 @@ const orderControllers = require("../controllers/orders");
 router.get("/", orderControllers.order_get_all);
 router.post("/", orderControllers.order_create);
 router.get("/:mail", orderControllers.order_get_mail);
-router.get("/#/:numeroOrden", orderControllers.order_get_orderNumber);
+router.get("/number/:orderNumber", orderControllers.order_get_orderNumber);
 router.delete(
   "/delete/:orderNumber",
   orderControllers.order_delete_orderNumber
