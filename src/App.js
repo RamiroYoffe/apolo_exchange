6186698ba6c8f6d62b1fa1fc95f898d160362698
Ryoffe <yofferamiro@gmail.com>
2021-02-27 17:10:32 -0300

    No major update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import PrivateRoute from './components/Auth/PrivateRoute'
 import { ProvideAuth } from './components/Auth/use-auth'
 import axios from 'axios'
+import './App.css'
 
 import Main from './components/pages/Main'
 import CustomNavbar from './components/CustomNavbar'
@@ -23,40 +24,48 @@ axios.defaults.withCredentials = true
 
 function App() {
 	return (
-		<ProvideAuth>
-			<CustomNavbar />
-			<Switch>
-				<Route exact path='/' component={Main} />
-				<Route exact path='/login' component={LogIn} />
-				<Route exact path='/signup' component={SignUp} />
-				<Route exact path='/orderConfirmed' component={OrderConfirmed} />
-				<PrivateRoute exact path='/account'>
-					<UserPage />
-				</PrivateRoute>
-				<PrivateRoute exact path='/manager'>
-					<Manager />
-				</PrivateRoute>
-				<PrivateRoute exact path='/manager/orders'>
-					<OrderList />
-				</PrivateRoute>
-				<PrivateRoute exact path='/manager/orders/:orderNum'>
-					<Order />
-				</PrivateRoute>
-				<PrivateRoute exact path='/manager/systems'>
-					<SystemsSettings />
-				</PrivateRoute>
-				<Route path='/manager/systems/:systemName' component={NewSystem} />
-				<PrivateRoute exact path='/manager/transactions'>
-					<TransactionSettings />
-				</PrivateRoute>
-				<PrivateRoute exact path='/manager/transactions/:transactionName'>
-					<NewTransaction />
-				</PrivateRoute>
-				<Route path='*'>
-					<NoMatch />
-				</Route>
-			</Switch>
-		</ProvideAuth>
+		<div style={{}}>
+			<ProvideAuth>
+				<CustomNavbar />
+				<Switch>
+					<Route exact path='/' component={Main} />
+					<Route exact path='/login' component={LogIn} />
+					<Route exact path='/signup' component={SignUp} />
+					<Route exact path='/orderConfirmed' component={OrderConfirmed} />
+					<PrivateRoute exact path='/account'>
+						<UserPage />
+					</PrivateRoute>
+					<PrivateRoute exact path='/manager'>
+						<Manager />
+					</PrivateRoute>
+					<PrivateRoute exact path='/manager/orders'>
+						<OrderList />
+					</PrivateRoute>
+					<PrivateRoute exact path='/manager/orders/:orderNum'>
+						<Order />
+					</PrivateRoute>
+					<PrivateRoute exact path='/manager/systems'>
+						<SystemsSettings />
+					</PrivateRoute>
+					<Route
+						path='/manager/systems/:systemName'
+						component={NewSystem}
+					/>
+					<PrivateRoute exact path='/manager/transactions'>
+						<TransactionSettings />
+					</PrivateRoute>
+					<PrivateRoute
+						exact
+						path='/manager/transactions/:transactionName'
+					>
+						<NewTransaction />
+					</PrivateRoute>
+					<Route path='*'>
+						<NoMatch />
+					</Route>
+				</Switch>
+			</ProvideAuth>
+		</div>
 	)
 }
 
