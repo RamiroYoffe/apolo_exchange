b051fa0119e769cc58c2e14426d31afbfbb70a96
LucianoNeimark <38950566+LucianoNeimark@users.noreply.github.com>
2021-02-26 23:57:22 -0300

    initial

--- backend/api/controllers/transactions.js
+++ backend/api/controllers/transactions.js
@@ -1,160 +1,160 @@
-const express = require('express')
+const express = require("express");
 
-const Transaction = require('../models/transaction')
-const mongoose = require('mongoose')
+const Transaction = require("../models/transaction");
+const mongoose = require("mongoose");
 
 exports.transaction_get_all = (req, res, next) => {
-	Transaction.find()
-		.exec()
-		.then((docs) => {
-			const response = {
-				Transactions: docs.map((doc) => {
-					return {
-						doc,
-					}
-				}),
-			}
-			res.status(200).json(response)
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).json({
-				error: err,
-			})
-		})
-}
+  Transaction.find()
+    .exec()
+    .then((docs) => {
+      const response = {
+        Transactions: docs.map((doc) => {
+          return {
+            doc,
+          };
+        }),
+      };
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 exports.transaction_get_one = (req, res, next) => {
-	Transaction.find({ name: req.params.name })
-		.exec()
-		.then((docs) => {
-			const response = {
-				Transaction: docs.map((doc) => {
-					return {
-						doc,
-					}
-				}),
-			}
-			res.status(200).json(response)
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).json({
-				error: err,
-			})
-		})
-}
+  Transaction.find({ name: req.params.name })
+    .exec()
+    .then((docs) => {
+      const response = {
+        Transaction: docs.map((doc) => {
+          return {
+            doc,
+          };
+        }),
+      };
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 exports.transaction_create = (req, res, next) => {
-	Transaction.find({ name: req.body.name })
-		.exec()
-		.then((transactionExistente) => {
-			if (transactionExistente.length > 0) {
-				//si el transaction existe:
-				return res.status(409).json({
-					message: 'This transaction already exists',
-				})
-			} else {
-				//crear el system nueva
-				const transaction = new Transaction({
-					_id: new mongoose.Types.ObjectId(),
-					name: req.body.name,
-					system1: req.body.system1,
-					system2: req.body.system2,
-					value: req.body.value,
-				})
-				transaction
-					.save()
-					.then((result) => {
-						res.status(201).json({
-							message: 'Transaction succesfully added',
-							name: result.name,
-							system1: result.system1,
-							system2: result.system2,
-							value: result.value,
-						})
-					})
-					.catch((err) => {
-						console.log(err)
-						res.status(500).json({
-							error: err,
-						})
-					})
-			}
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).json({
-				error: err,
-			})
-		})
-}
+  Transaction.find({ name: req.body.name })
+    .exec()
+    .then((transactionExistente) => {
+      if (transactionExistente.length > 0) {
+        //si el transaction existe:
+        return res.status(409).json({
+          message: "This transaction already exists",
+        });
+      } else {
+        //crear el system nueva
+        const transaction = new Transaction({
+          _id: new mongoose.Types.ObjectId(),
+          name: req.body.name,
+          system1: req.body.system1,
+          system2: req.body.system2,
+          value: req.body.value,
+        });
+        transaction
+          .save()
+          .then((result) => {
+            res.status(201).json({
+              message: "Transaction succesfully added",
+              name: result.name,
+              system1: result.system1,
+              system2: result.system2,
+              value: result.value,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 exports.transaction_delete_name = (req, res, next) => {
-	Transaction.find({ name: req.params.name })
-		.exec()
-		.then((transactionEncontrada) => {
-			if (transactionEncontrada.length > 0) {
-				Transaction.remove({ name: req.params.name })
-					.exec()
-					.then((transactionExistente) => {
-						res.status(200).json({
-							message: 'Transaction succesfully eliminated',
-							result: transactionExistente,
-						})
-					})
-					.catch((err) => {
-						console.log(err)
-						res.status(500).json({
-							error: err,
-						})
-					})
-			} else {
-				return res.status(409).json({
-					message: 'Transaction not found',
-				})
-			}
-		})
-}
+  Transaction.find({ name: req.params.name })
+    .exec()
+    .then((transactionEncontrada) => {
+      if (transactionEncontrada.length > 0) {
+        Transaction.remove({ name: req.params.name })
+          .exec()
+          .then((transactionExistente) => {
+            res.status(200).json({
+              message: "Transaction succesfully eliminated",
+              result: transactionExistente,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          });
+      } else {
+        return res.status(409).json({
+          message: "Transaction not found",
+        });
+      }
+    });
+};
 
 exports.transaction_modify_value = (req, res, next) => {
-	const name = req.params.name
-	const updateOps = {}
-	for (const ops of req.body) {
-		updateOps[ops.propName] = ops.value
-	}
-	Transaction.find({ name: req.params.name })
-	Transaction.update({ name: name }, { $set: updateOps })
-		.exec()
-		.then((result) => {
-			console.log(result)
-			res.status(200).json({
-				message: 'Transaction Updated',
-			})
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).json({ error: err })
-		})
-}
+  const name = req.params.name;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Transaction.find({ name: req.params.name });
+  Transaction.update({ name: name }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "Transaction Updated",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+};
 
 exports.transaction_get_id = (req, res, next) => {
-	Transaction.find({ _id: req.params.id })
-		.exec()
-		.then((docs) => {
-			const response = {
-				Transactions: docs.map((doc) => {
-					return {
-						doc,
-					}
-				}),
-			}
-			res.status(200).json(response)
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).json({
-				error: err,
-			})
-		})
-}
+  Transaction.find({ _id: req.params.id })
+    .exec()
+    .then((docs) => {
+      const response = {
+        Transactions: docs.map((doc) => {
+          return {
+            doc,
+          };
+        }),
+      };
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
--- backend/api/controllers/users.js
+++ backend/api/controllers/users.js
@@ -1,130 +1,135 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
-const mongoose = require('mongoose')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const mongoose = require("mongoose");
 
 exports.user_get_all = (req, res, next) => {
-	User.find()
-		.exec()
-		.then((docs) => {
-			const response = {
-				monedas: docs.map((doc) => {
-					return {
-						doc,
-					}
-				}),
-			}
-			res.status(200).json(response)
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).json({
-				error: err,
-			})
-		})
-}
+  User.find()
+    .exec()
+    .then((docs) => {
+      const response = {
+        monedas: docs.map((doc) => {
+          return {
+            doc,
+          };
+        }),
+      };
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 exports.user_register = (req, res, next) => {
-	const id = new mongoose.Types.ObjectId()
-	// let mailOptions = {
-	//   // should be replaced with real recipient's account
-	//   to: req.body.mail,
-	//   subject: "Eatit - Verificacion de cuenta",
-	//   text:
-	//     "Para verificar tu cuenta visita:" +
-	//     "https://eatit.com.ar/verify/?=" +
-	//     id,
-	// };
-	User.find({ mail: req.body.mail })
-		.exec()
-		.then((userFound) => {
-			if (userFound.length > 0) {
-				return res.status(409).json({
-					message: 'mail exists',
-				})
-			} else {
-				bcrypt.hash(req.body.password, 10, (err, hash) => {
-					if (err) {
-						return res.status(500).json({
-							error: err,
-						})
-					} else {
-						const user = new User({
-							_id: id,
-							cbu: req.body.cbu,
-							cuil: req.body.cuil,
-							bank: req.body.bank,
-							user_name: req.body.user_name,
-							account_name: req.body.account_name,
-							mail: req.body.mail,
-							password: hash,
-							orders: [],
-						})
+  const id = new mongoose.Types.ObjectId();
+  // let mailOptions = {
+  //   // should be replaced with real recipient's account
+  //   to: req.body.mail,
+  //   subject: "Eatit - Verificacion de cuenta",
+  //   text:
+  //     "Para verificar tu cuenta visita:" +
+  //     "https://eatit.com.ar/verify/?=" +
+  //     id,
+  // };
+  User.find({ mail: req.body.mail })
+    .exec()
+    .then((userFound) => {
+      if (userFound.length > 0) {
+        return res.status(409).json({
+          message: "mail exists",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              _id: id,
+              cbu: req.body.cbu,
+              cuil: req.body.cuil,
+              bank: req.body.bank,
+              user_name: req.body.user_name,
+              account_name: req.body.account_name,
+              mail: req.body.mail,
+              password: hash,
+              orders: [],
+            });
 
-						user
-							.save()
-							.then((result) => {
-								const token = jwt.sign(
-									{
-										mail: result.mail,
-										userId: result._id,
-									},
-									'secret',
-									{
-										expiresIn: '1h',
-									}
-								)
-								res.cookie('token', token, { httpOnly: true }).send()
-							})
-							.catch((err) => {
-								console.log(err)
-								res.status(500).json({
-									error: err,
-								})
-							})
-					}
-				})
-			}
-		})
-}
+            user
+              .save()
+              .then((result) => {
+                const token = jwt.sign(
+                  {
+                    mail: result.mail,
+                    userId: result._id,
+                  },
+                  "secret",
+                  {
+                    expiresIn: "1h",
+                  }
+                );
+                res.cookie("token", token, { httpOnly: true }).send();
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
+};
+
+exports.user_Islogin = (req, res, next) => {
+  res.status(200).json({
+    Message: "Still Logged In",
+  });
+};
 
 exports.user_login = (req, res, next) => {
-	User.find({ mail: req.body.mail })
-		.exec()
-		.then((usr) => {
-			if (usr.length < 1) {
-				// No existe el usuario
-				return res.status(401).json({
-					message: 'Auth failed -- NO EXISTE EL USUARIO',
-				})
-			}
+  User.find({ mail: req.body.mail })
+    .exec()
+    .then((usr) => {
+      if (usr.length < 1) {
+        // No existe el usuario
+        return res.status(401).json({
+          message: "Auth failed -- NO EXISTE EL USUARIO",
+        });
+      }
 
-			bcrypt.compare(req.body.password, usr[0].password, (err, result) => {
-				if (result) {
-					//si result = true es que la pass es correcta
-					const token = jwt.sign(
-						{
-							mail: usr[0].mail,
-							userId: usr[0]._id,
-						},
-						'secret',
-						{
-							expiresIn: '1h',
-						}
-					)
-					res.cookie('token', token, { httpOnly: true }).send()
-				} else {
-					res.status(401).json({ message: 'Auth Failed' })
-				}
-			})
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).json({
-				error: err,
-			})
-		})
-}
+      bcrypt.compare(req.body.password, usr[0].password, (err, result) => {
+        if (result) {
+          //si result = true es que la pass es correcta
+          const token = jwt.sign(
+            {
+              mail: usr[0].mail,
+              userId: usr[0]._id,
+            },
+            "secret",
+            {
+              expiresIn: "1h",
+            }
+          );
+        } else {
+          res.status(401).json({ message: "Auth Failed" });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 //https://mongoosejs.com/docs/populate.html#setting-populated-fields  --> clave
 //
